e69a1b353ad87d16a91fc3f2e2c63e19d7285278
Mackenzie Zastrow <zastrowm+bitbucket@gmail.com>
2015-09-29 20:42:21 -0500

    Ignore files that shouldn't have been added.

--- app.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    var contentElement = document.getElementById('content');
-    var doc = new DocumentOwner();
-    doc.attachTo(contentElement);
-};
-//# sourceMappingURL=app.js.map
