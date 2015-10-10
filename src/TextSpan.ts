ec0f9850b6f029f874c3da23f476b23ea847119c
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2015-10-10 18:31:11 -0500

    Add deletion support to TextSpan

--- src/TextSpan.ts
+++ src/TextSpan.ts
@@ -29,7 +29,10 @@ class TextSpan {
     return this.parent.spans[this.childIndex + 1];
   }
 
-  insertText(relativeInsertionPoint: number, text: string) {
+  /**
+   * Insert text into the span at the designated location.
+   */
+  public insertText(relativeInsertionPoint: number, text: string) {
     let element = this._element;
     var textContent = element.textContent;
 
@@ -41,10 +44,37 @@ class TextSpan {
     this._length += text.length;
   }
 
-  getText() {
+  /**
+   * Remove the given number of characters from the span.
+   */
+  public removeText(relativeDeletionPoint: number, numberOfCharactersToRemove: number): string {
+    let element = this._element;
+    var textContent = element.textContent;
+
+    var removed = textContent.substr(relativeDeletionPoint, numberOfCharactersToRemove);
+
+    // OPTIMIZE/PROFILE - if we're at the end of the substring, we can avoid the concat
+    element.textContent = textContent.substr(0, relativeDeletionPoint)
+      + textContent.substr(relativeDeletionPoint + numberOfCharactersToRemove);
+
+    // TODO count unicode instead of characters?
+    this._length = this._length - removed.length
+
+    return removed;
+  }
+
+  /**
+   * Get the text contained in the given span. 
+   */
+  public getText() {
+    // PROFILE - should we cache this?
     return this._element.textContent;
   }
 
+  /**
+   * Provide a human-readable json object used for quickly identifying
+   * the contents of the span
+   */
   private dump(): any {
     return {
       childIndex: this.childIndex,
