b62b8f69694e95b5078e29ee09d97603781b4992
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2015-10-11 20:47:16 -0500

    Prepare default file for live editing of the document

--- src/DocumentOwner.ts
+++ src/DocumentOwner.ts
@@ -3,15 +3,15 @@
   private _undoStack: UndoStack = new UndoStack();
   private _blockMap: Map<UniqueId, Block> = new Map<UniqueId, Block>();
 
-  private _root: ContainerBlock;
+  public root: ContainerBlock;
 
   constructor() {
-    this._root = new ContainerBlock(new TextBlock());
+    this.root = new ContainerBlock(new TextBlock());
   }
 
   attachTo(element: HTMLElement) {
     element.innerHTML = "";
-    element.appendChild(this._root.getElement());
+    element.appendChild(this.root.getElement());
   }
 
   public get undo(): UndoStack {
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 ﻿
 
 window.onload = () => {
-  var contentElement = document.getElementById('content');
+  var contentElement = document.getElementById('document');
+
+  var originalText = contentElement.textContent;
 
   var doc = new DocumentOwner();
   doc.attachTo(contentElement);
+
+  (<TextBlock>doc.root.children[0]).spans[0].insertText(0, originalText);
 };
